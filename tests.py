7aed1d0ba68e3e47e7b484ad49027f3f611ec77d
Евгений <jako.soll@gmail.com>
2020-11-19 21:51:49 +0300

    Update files

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 from task_file_handler import cut_title
 
-title = '-1234-1234-1234-1234-1234-1234-1234-1234-1234-1234'
+title = '-1234-1234-1234-1234-1234-1234-1234-1234-1234-1234-'
 print(len(title))
 
 print(cut_title(title))
